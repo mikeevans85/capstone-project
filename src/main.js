8fec0933a4abdb526b0a9fec5ae12214c952778b
mikeevans85 <baudecordier@gmail.com>
2018-12-10 21:20:38 -0600

    Edit check

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import ToggleButton from "vue-js-toggle-button";
+import Vue2Filters from "vue2-filters";
+
+Vue.use(Vue2Filters);
 
 Vue.use(ToggleButton);
 
